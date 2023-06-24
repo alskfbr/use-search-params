3f805bb32eefc1ac1e4c3be12041b4d37b43143f
Ming Han <minghan_geh@cloudplus.com.sg>
2023-06-25 01:28:24 +0800

    initial commit

---
+++ src/hooks/useSearchParams.js
@@ -0,0 +1,32 @@
+import { useSearchParams as useRRDSearchParams } from "react-router-dom";
+import queryString from "query-string";
+
+/**
+ * Extended from `useSearchParams` hook from "react-router-dom". To
+ * eliminate the empty value, parse number value, boolean value and array value.
+ * @param {object} defaultValue Default value of search params.
+ */
+export function useSearchParams(defaultValue) {
+  const [urlSearchParams, setUrlSearchParams] = useRRDSearchParams(defaultValue);
+  const urlSearchParamsObject = Object.entries([...urlSearchParams]);
+
+  const stringifyObject = (objectValue) => {
+    const config = { arrayFormat: "index", skipEmptyString: true, skipNull: true };
+    return queryString.stringify(objectValue, config);
+  };
+
+  const objectifyString = (stringValue) => {
+    const config = { arrayFormat: "index", parseBoolean: true, parseNumbers: true };
+    return queryString.parse(stringValue, config);
+  };
+
+  const setSearchParams = (objectValue, navigationOpts) => {
+    const queryParams = stringifyObject(objectValue); // To eliminate empty value.
+    setUrlSearchParams(queryParams, navigationOpts);
+  };
+
+  const searchParamsAsString = stringifyObject(urlSearchParamsObject);
+  const searchParamsAsObject = objectifyString(searchParamsAsString);
+
+  return [searchParamsAsString, searchParamsAsObject, setSearchParams];
+}
